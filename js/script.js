2708ffdf361b7b5eb56d92f8bddf27ae3aeb0950
NycollSM <nycollsotomorales21@gmail.com>
2018-11-05 02:20:28 -0600

    prueba de buscador sin terminar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 const content = document.getElementById('container');
+const searching = document.getElementById('search');
+const txt = document.getElementById('txt');
 
-function obtenerDatos (){
-    let url = 'https://got-quotes.herokuapp.com/quotes';
-    const api = new XMLHttpRequest();
-    api.open('GET', url, true);
-
-
-
-    api.send();
+function contentApi(){
+    if (txt !== ''){
+    var newTxt = txt.value;
+    var paragraph = document.createElement('p'); 
+    paragraph.innerHTML += newTxt;
+    content.appendChild(parragrah); 
+    } 
+    else{
+        console.log('error');
+    }
 }
+searching.addEventListener('submit', contentApi);
+
+
+
+//function obtenerDatos (){
+    //let url = 'https://got-quotes.herokuapp.com/quotes';
+  //  const api = new XMLHttpRequest();
+  //  api.open('GET', url, true);
+  //  api.send();
+//}
