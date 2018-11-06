b8505499b5643f189ae5c76629f9049eb64acbba
NycollSM <nycollsotomorales21@gmail.com>
2018-11-06 17:55:31 -0600

    agregando imagenes al dom

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ const content = document.getElementById('container');
 const searching = document.querySelector('button');
 const txt = document.getElementById('txt');
 
+const request = new XMLHttpRequest();
 
+// agrega los elementos al dom
+function imagesAPI (event){
+  const response = event.target.response;
+  console.log(response);
+
+  for (const i of response.results){
+    const list = document.getElementById('list');
+    const listImage = document.createElement('li');
+    listImage.innerHTML = i.image;
+    list.appendChild(listImage);
+    const images = document.createElement('img');
+    images.setAttribute = ('src', i.image);
+    listImage.appendChild(images);
+    console.log(images);
+  }
+}
+request.addEventListener('load', imagesAPI);
+console.log(imagesAPI);
+
+// buscador
 function contentApi(){
         let a = document.createElement('p');
         a.innerHTML += txt.value;
@@ -11,13 +32,9 @@ function contentApi(){
         console.log(txt.innerHTML);
         txt.value = '';
 }
-searching.addEventListener('click', contentApi);
+//searching.addEventListener('click', obtenerDatos);
 
-
-
-//function obtenerDatos (){
-    //let url = 'https://got-quotes.herokuapp.com/quotes';
-  //  const api = new XMLHttpRequest();
-  //  api.open('GET', url, true);
-  //  api.send();
-//}
+// respuesta tipo json 
+request.responseType = 'json';
+request.open('GET', 'https://rickandmortyapi.com/api/character');
+request.send();
