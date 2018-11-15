6fd999042c8df7c693b29e4dd88592665e1286c5
NycollSM <nycollsotomorales21@gmail.com>
2018-11-15 06:45:46 -0600

    nombre de personajes

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,46 @@
-
 const content = document.getElementById('container');
+let imagesResponse;
+let t;
 // const searching = document.querySelector('button')
-const txt = document.getElementById('txt');
-
 const request = new XMLHttpRequest();
+const secondrequest = new XMLHttpRequest();
 
-// agrega los elementos al dom
-function imagesAPI(event) {
+function Information (event){
   const response = event.target.response;
   console.log(response);
-
-  for (let i of response.results){
-    const imagesResponse = document.createElement('img');
-    imagesResponse.setAttribute('src', i.image);
-  // agrego elementos al dom
-    content.appendChild(imagesResponse);
+  for (let info of response.results){
+    const infotxt = info.response.results;
+    const txtdom = document.createElement('p');
+    txtdom.innerText = infotxt;
   }
 }
-request.addEventListener('load', imagesAPI);
+
+function imagesAPI(event) {
+  const response = event.target.response;
+  for (let item of response.results){
+    imagesResponse = document.createElement('img');
+    imagesResponse.setAttribute('src', item.image);
+    imagesResponse.addEventListener('click', Information);
+    //nombre de personaje
+    t = document.createElement('p');
+    t.innerHTML = item.name;
+  // agrego elementos al dom
+    content.appendChild(imagesResponse);
+    content.appendChild(t);
+  }
+}
+
+
+
 
 // respuesta tipo json
+// primer request
 request.responseType = 'json';
 request.open('GET', 'https://rickandmortyapi.com/api/character/');
 request.send();
+request.addEventListener('load', imagesAPI);
+//second request
+secondrequest.responseType = 'json';
+secondrequest.open('GET', 'https://rickandmortyapi.com/api/location/');
+secondrequest.send();
+secondrequest.addEventListener('load', Information);
